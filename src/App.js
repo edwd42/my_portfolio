d8c675cac2c6cdae69fd8042e667c2e4c34e250b
edwd42 <edwd42@yahoo.com>
2019-06-22 19:56:53 -0400

    add react-router-dom and configure routes in main.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/main";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         </Drawer>
         <Content>
           <div className="page-content" />
+          <Main />
         </Content>
       </Layout>
     </div>
