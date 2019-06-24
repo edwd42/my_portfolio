ae3952af85aa9725c01214cff1b354294392fa35
edwd42 <edwd42@yahoo.com>
2019-06-24 16:54:37 -0400

    add <Tabs> and toggleCategories() method to projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
+import { Tabs, Tab, Grid, Cell } from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          <h1>React Page</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is Angular</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is VueJS</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Projects Page</h1>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJS</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
